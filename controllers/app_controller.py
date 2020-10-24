61da0187526f39ba7fb5980b8b08299790ea786e
QI-D <jonsondang@outlook.com>
2020-10-23 21:11:35 -0700

    creating

---
+++ controllers/app_controller.py
@@ -0,0 +1,11 @@
+from models.maze import Maze
+
+from .welcome_controller import WelcomeController
+from .game_controller import GameController
+from .game_over_controller import GameOverController
+
+class App:
+    
+
+    def run(self):
+        pass
