137838cb893b126922cd0e88b1f2a972bb33d018
Tom Han <tomhyhan@gmail.com>
2020-11-08 19:51:50 -0800

    pygame interface added

--- controllers/app_controller.py
+++ controllers/app_controller.py
@@ -1,8 +1,15 @@
 from models.maze import Maze
+from models.grid_size import GridSize
 
 from controllers.welcome_controller import WelcomeController
 from controllers.game_controller import GameController
 from controllers.game_over_controller import GameOverController
+
+import pygame
+import pygame.locals
+
+from models.player import Player
+
 
 class App:
     '''Main class for the application. It interacts with Maze model and three controllers:
@@ -14,7 +21,7 @@ class App:
         """ Call the classmethod to load all students from the CSV """
         self._maze = Maze()
         self._maze._load_all_from_file(filename)
-       
+        self._maze.generate_random_spots()
 
     def run(self):
         """ This is the main method for our application.
@@ -23,24 +30,73 @@ class App:
         The `SystemExit` exception is raised by the child controllers.
 
         """
+        pygame.init()
+        print(self._maze.locations)
+        window = pygame.display.set_mode(
+            (self._maze.row * GridSize.SIZE, self._maze.col * GridSize.SIZE + 50))
+        clock = pygame.time.Clock()
 
         welcome_controller = WelcomeController()
 
         running = True
 
+        self._maze.create_player()
+        self._maze.create_maze_exit()
+        wall = self._maze.create_wall()
+        items = self._maze.create_items()
+
         welcome_controller.run()
-        welcome_controller.get_input()
+        # welcome_controller.get_input()
+        score = 0
 
         while running:
-            
-            game_controller = GameController(self._maze)
-            
+            clock.tick(20)
+            window.fill((0, 0, 0))
+
+            for event in pygame.event.get():
+                if event.type == pygame.locals.QUIT:
+                    running = False
+
+            game_controller = GameController(self._maze, self._maze.player)
+
             result = game_controller.run()
 
-            if result:
-                running=False
+            if pygame.sprite.spritecollide(self._maze.player, items, dokill=True):
+                score += 1
+
+            # move these to view?
+            window.blit(create_text_surface(
+                f"Score: {score}"), (self._maze.row * GridSize.SIZE - GridSize.SIZE * 2, self._maze.col * GridSize.SIZE))
+            window.blit(create_text_surface(
+                f"{pygame.time.get_ticks()} ms"), (0, self._maze.col * GridSize.SIZE))
+
+            # move these to view?
+            window.blit(self._maze.player.image, self._maze.player.rect)
+            window.blit(self._maze.maze_exit.image, self._maze.maze_exit.rect)
+            wall.draw(window)
+            items.draw(window)
+
+            if pygame.sprite.collide_rect(self._maze.player, self._maze.maze_exit):
+                # game over controller will be in here
+                running = False
+
+            pygame.display.update()
+
+            #
+
+            # if result:
+            #     running = False
 
         # result can be win or loss
 
-        game_over_controller=GameOverController(self._maze)
-        game_over_controller.run()
+        # game_over_controller = GameOverController(self._maze)
+        # game_over_controller.run()
+
+# better to put this somewhere else
+
+
+def create_text_surface(text):
+    openSans = pygame.font.SysFont('open sans', 24)
+    text_surface = openSans.render(text, True, (160, 0, 0))
+
+    return text_surface
--- controllers/game_controller.py
+++ controllers/game_controller.py
@@ -1,5 +1,8 @@
 from views.game_view import GameView
 from .keyboard_controller import KeyboardController
+from models.grid_size import GridSize
+
+import pygame
 
 
 class GameController:
@@ -7,39 +10,59 @@ class GameController:
        Call functions from Maze to get position of player, items, and exit
        Set up how player will move based on "w,a,s,d" command'''
 
-    def __init__(self, maze):
+    def __init__(self, maze, player):
         self._maze = maze
+        self.player = player
         self._game_over = False
-        self._maze.generate_random_spots()
         self._view = GameView(maze)
         self._keyboard_controller = KeyboardController()
-
 
     @property
     def player_current_location(self):
         return (self._maze.locations["P"][0]+self._maze.movements_player[0], self._maze.locations["P"][1]+self._maze.movements_player[1])
 
-
     def run(self):
+        player_rol = self.player_current_location[0]
+        player_col = self.player_current_location[1]
+        keys = pygame.key.get_pressed()
+        if keys[pygame.locals.K_RIGHT]:
+            if player_col + 1 < self._maze.row and self._maze.check_position(player_rol, player_col + 1):
+                self.move_with_input("d")
+                self._maze.player.rect.x = min(
+                    self._maze.player.rect.x + GridSize.SIZE, self._maze.row * GridSize.SIZE - GridSize.SIZE)
+        elif keys[pygame.locals.K_LEFT]:
+            self.move_with_input("a")
+            if self._maze.check_position(player_rol, player_col - 1):
+                self.player.rect.x = max(
+                    self.player.rect.x - GridSize.SIZE, 0)
+        elif keys[pygame.locals.K_UP]:
+            if self._maze.check_position(player_rol - 1, player_col):
+                self.move_with_input("w")
+                self.player.rect.y = max(
+                    self.player.rect.y - GridSize.SIZE, 0)
+        elif keys[pygame.locals.K_DOWN]:
+            if player_rol + 1 < self._maze.col and self._maze.check_position(player_rol + 1, player_col):
+                self.move_with_input("s")
+                self.player.rect.y = min(
+                    self.player.rect.y + GridSize.SIZE, self._maze.col * GridSize.SIZE - GridSize.SIZE)
 
-        # call display_maze() to display the maze
-        self._view.display_maze()
+        # # call display_maze() to display the maze
+        # self._view.display_maze()
 
-        while not self._game_over:
-            # giving player insturctions of commands
-            self._view.display_move_options()
+        # while not self._game_over:
+        #     # giving player insturctions of commands
+        #     self._view.display_move_options()
 
-            user_input = self._keyboard_controller.get_action()
+        #     user_input = self._keyboard_controller.get_action()
 
-            self.move_with_input(user_input)
+        #     self.move_with_input(user_input)
 
-            # if 'P' reaches the exit, game over condition triggered
-            if self._maze.is_exit(self.player_current_location):
-            # if self._maze.is_exit(self.maze.movements.player):
-                self._game_over=True
-            
-        return self._game_over
+        #     # if 'P' reaches the exit, game over condition triggered
+        #     if self._maze.is_exit(self.player_current_location):
+        #         # if self._maze.is_exit(self.maze.movements.player):
+        #         self._game_over = True
 
+        # return self._game_over
 
     def move_with_input(self, user_input):
         movement = {
@@ -48,14 +71,13 @@ class GameController:
             "a": (0, -1),
             "d": (0, 1)
         }
-        
+
         # position will not change if user_input is not a recognized command
         shift = movement.get(user_input, (0, 0))
-        
+
         x = shift[0]
         y = shift[1]
         self._maze.move_player(x, y)
         self._view.display_maze()
         if self._maze.get_item():
             self._view.get_items(self._maze.player.backpack)
-    
